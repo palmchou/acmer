1f1dfe27db45cacb3dd295bd5f92d1020799fa9f
Palm <palmchou@live.com>
2014-05-05 14:13:03 +0800

    Create router 'down', use for submit and post the worked out  problems .

---
+++ routes/down.js
@@ -0,0 +1,13 @@
+/**
+ * Created by palm on 14-5-5.
+ */
+var express = require('express');
+var router = express.Router();
+
+function doPost(req, res){
+
+}
+
+router.post('/', doPost);
+
+module.exports = router;
